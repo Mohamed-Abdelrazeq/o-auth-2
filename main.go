09496c67e58b61eb8f86f892fcdfa5cf9426feed
Mohamed <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2024-06-18 08:29:54 +0300

    load env

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load Environment Variables
+	loadEnv()
+
+	// Create Router
 	r := gin.Default()
+
+	// Test Router
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(
 			200,
@@ -14,5 +23,13 @@ func main() {
 			})
 	})
 
+	// Run Server
 	r.Run()
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
