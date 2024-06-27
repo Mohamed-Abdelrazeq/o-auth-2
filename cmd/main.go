8fc67f16ce18b8b0d4ac1ac0f4611f7d60571b33
Mohamed <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2024-06-27 20:33:33 +0300

    separate handlers

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,9 @@ import (
 	apps "Mohamed-Abdelrazeq/o-auth-2/internal/application"
 	"Mohamed-Abdelrazeq/o-auth-2/internal/handlers"
 	"Mohamed-Abdelrazeq/o-auth-2/internal/loaders"
-	"Mohamed-Abdelrazeq/o-auth-2/internal/models"
 	"Mohamed-Abdelrazeq/o-auth-2/internal/services"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -20,6 +17,7 @@ func main() {
 	// Init AuthService
 	authService := services.NewAuthSericeInstance(db)
 	authApp := apps.NewAuthApplicationInstance(authService)
+	authHandler := handlers.NewAuthHandlerInstance(authApp)
 
 	// Create Router
 	r := gin.Default()
@@ -28,58 +26,8 @@ func main() {
 	r.GET("/ping", handlers.Ping)
 
 	// Auth Router
-	r.POST("/login", func(ctx *gin.Context) {
-		// Binding
-		var loginUserParams models.LoginUserParams
-		ctx.Bind(&loginUserParams)
-		// Validation
-		validate := validator.New()
-		if err := validate.Struct(loginUserParams); err != nil {
-			ctx.JSON(
-				http.StatusBadRequest,
-				models.ErrorMap{Message: "invalid body"},
-			)
-			return
-		}
-		token, err := authApp.Login(loginUserParams)
-		if err != nil {
-			ctx.JSON(
-				http.StatusBadRequest,
-				models.ErrorMap{Message: "invalid credintials"},
-			)
-			return
-		}
-
-		// Return
-		ctx.JSON(200, token)
-	})
-
-	r.POST("/register", func(ctx *gin.Context) {
-		// Binding
-		var createUserParams models.CreateUserParams
-		ctx.Bind(&createUserParams)
-		// Validation
-		validate := validator.New()
-		if err := validate.Struct(createUserParams); err != nil {
-			ctx.JSON(
-				http.StatusBadRequest,
-				models.ErrorMap{Message: "invalid body"},
-			)
-			return
-		}
-		//
-		token, err := authApp.Register(createUserParams)
-		if err != nil {
-			ctx.JSON(
-				http.StatusBadRequest,
-				models.ErrorMap{Message: "invalid credintials"},
-			)
-			return
-		}
-
-		// Return
-		ctx.JSON(200, token)
-	})
+	r.POST("/login", authHandler.Login)
+	r.POST("/register", authHandler.Register)
 
 	// Run Server
 	r.Run()
--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -1,5 +1,79 @@
 package handlers
 
+import (
+	apps "Mohamed-Abdelrazeq/o-auth-2/internal/application"
+	"Mohamed-Abdelrazeq/o-auth-2/internal/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type AuthHandler interface {
+	Login(ctx *gin.Context)
+	Register(ctx *gin.Context)
+}
+
+type AuthHandlerInstance struct {
+	authApp apps.AuthApplication
+}
+
+func NewAuthHandlerInstance(authApp apps.AuthApplication) AuthHandlerInstance {
+	return AuthHandlerInstance{authApp: authApp}
+}
+
 // Register With Email & Password
+func (authHandler AuthHandlerInstance) Register(ctx *gin.Context) {
+	// Binding
+	var createUserParams models.CreateUserParams
+	ctx.Bind(&createUserParams)
+	// Validation
+	validate := validator.New()
+	if err := validate.Struct(createUserParams); err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			models.ErrorMap{Message: "invalid body"},
+		)
+		return
+	}
+	//
+	token, err := authHandler.authApp.Register(createUserParams)
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			models.ErrorMap{Message: "invalid credintials"},
+		)
+		return
+	}
+
+	// Return
+	ctx.JSON(200, token)
+}
 
 // Login With Email & Password
+func (authHandler AuthHandlerInstance) Login(ctx *gin.Context) {
+
+	// Binding
+	var loginUserParams models.LoginUserParams
+	ctx.Bind(&loginUserParams)
+	// Validation
+	validate := validator.New()
+	if err := validate.Struct(loginUserParams); err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			models.ErrorMap{Message: "invalid body"},
+		)
+		return
+	}
+	token, err := authHandler.authApp.Login(loginUserParams)
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			models.ErrorMap{Message: "invalid credintials"},
+		)
+		return
+	}
+
+	// Return
+	ctx.JSON(200, token)
+}
